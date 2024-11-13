d2793ea629217d19256c6a52aacfa2f942b722a7
Carlos André <161266479+carlosandret@users.noreply.github.com>
2024-11-13 15:30:52 -0300

    Update ex2_relogio_simples.py

--- TDS_2/Orientação a Objetos-POO/ex2_relogio_simples.py
+++ TDS_2/Orientação a Objetos-POO/ex2_relogio_simples.py
@@ -1,31 +1,48 @@
-# 1. Exercitando o processo de abstração, modele uma classe Relógio_Digital_Simples com seus estados e
-# comportamentos. Crie a respectiva classe em python e depois crie 2 objetos, imprima os valores de seus
-# atributos e execute os métodos criados. Recomendação: criar estados que possam ter seus valores alterados
-# por seus métodos.
+class Relógio_Digital_Simples:
+    def __init__(self, hora, minuto):
+        self.horas = hora
+        self.minutos = minuto
 
-class Relogio_Digital_Simples:
-        def __init__(self, hora, minuto):
-            self.hora = hora
-            self.minuto = minuto
-            
+    def alterar_hora(self):
+        nova_hora = int(input('Digite o novo horário (no formato 00): '))
+        if 0 <= nova_hora <= 24:
+            self.horas = nova_hora
+        else: 
+            print('Horário inválido. Tente novamente.')
+            return self.alterar_hora()
+
+    def alterar_minuto(self):
+        novo_minuto = int(input('Digite os novos minutos: '))
+        if 0 <= novo_minuto <= 60:
+            self.minutos = novo_minuto
+        else:
+            print('Horário inválido. Tente novamente.')
+            return self.alterar_minuto()
+
 def main():
+    hora = int(input('Digite as horas (no formato 00): '))
+    minuto = int(input('Digite os minutos (no formato 00): '))
+    if 0<= hora <= 24 and 0<= minuto <= 60:
+        meu_relogio = Relógio_Digital_Simples(hora, minuto)
+        print(f'Seu relógio: {meu_relogio.horas}:{meu_relogio.minutos}')
+    else:
+        print('Número inválido. Tente novamente!!')
+        return main()
     while True:
-        opcao = int(input('Escolha uma das opções: '))
-    
-    
-    
-if __name__ =="__main__":
+        print('\n')
+        opcao = int(input(''' Escolha uma das opções:
+[ 1 ] mudar hora
+[ 2 ] alterar minutos
+[ 3 ] encerrar                                                  
+'''))
+        if opcao == 1:
+            meu_relogio.alterar_hora()
+        elif opcao == 2:
+            meu_relogio.alterar_minuto()
+        elif opcao == 3:
+            print('Terminando...')
+            break
+        print(f'SEU RELÓGIO: {meu_relogio.horas}:{meu_relogio.minutos}')
+            
+if __name__=='__main__':
     main()
-# class Gato:
-#   rg = 0
-#   def __init__(self,nome,peso,idade):
-#     Gato.rg += 1
-#     self.rg = Gato.rg
-#     self.nome = nome
-#     self.peso = peso
-#     self.idade = idade
-    
-# mimi = Gato("Mimi",0.1,0)
-# print(f'Rg:{mimi.rg} Nome: {mimi.nome}')
-# tom = Gato("Tom",0.5,1)
-# print(f'Rg:{tom.rg} Nome: {tom.nome}')
