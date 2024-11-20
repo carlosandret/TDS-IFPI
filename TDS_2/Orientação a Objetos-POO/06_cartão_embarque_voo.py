004a3bb726bd10651c2f7746eb974c3b987a3fc4
Carlos André <161266479+carlosandret@users.noreply.github.com>
2024-11-20 11:38:50 -0300

    Update 06_cartão_embarque_voo.py

--- TDS_2/Orientação a Objetos-POO/06_cartão_embarque_voo.py
+++ TDS_2/Orientação a Objetos-POO/06_cartão_embarque_voo.py
@@ -2,12 +2,14 @@
 # cartão de embarque de voo. Identifique atributos mutáveis e imutáveis, implemente um
 # construtor da classe e métodos para manipulação dos atributos mutáveis. Faça todas as
 # validações possíveis. Crie objetos para testar os métodos implementados.
-
 from random import *
 
 def assentos_do_voo():
-    return randint(10, 111)
-
+    assentos = []
+    for c in range(5):
+        assentos.append(randint(1, 100))
+    return assentos
+    
 class Cartao_embarque:
     def __init__(self, nome_passageiro, numero_voo, codigo_reserva, data, hora, status_checkin = 'Não realizado', num_assento = 'Não selecionado'):
         self.nome_passageiro = nome_passageiro
@@ -20,14 +22,17 @@ class Cartao_embarque:
     
     def realiza_checkin(self):
         self.status_checkin = 'Realizado'
-        self.num_assento = assentos_do_voo()
-        self.assentos = []
-        self.assentos.append(self.num_assento)
+        self.assentos = assentos_do_voo()
+        self.num_assento = choice(self.assentos)
 
     def altera_assento(self, n_assento):
         if self.status_checkin == 'Realizado':
             if self.confere_assento_disponivel(n_assento):
                 self.num_assento = n_assento
+            else:
+                return 'Este assento não está disponível'
+        else:
+            return 'Realize o check-in antes de trocar de assento'
     
     def confere_assento_disponivel(self, n_assento):
         return n_assento not in self.assentos
@@ -37,12 +42,34 @@ class Cartao_embarque:
         s2 = f'\nData e hora de embarque: {self.data} às {self.hora} horas\nStatos do Check-in: {self.status_checkin}\nNúmero assento: {self.num_assento}'
         return s1+s2
 
-def main():
-    cartao_1 = Cartao_embarque('Carlos André', 13, 5, '13/01/2024', '12:00')
-    cartao_2 = Cartao_embarque('Carlos André', 13, 5, '13/01/2024', '12:00')
-    cartao_3 = Cartao_embarque('Carlos André', 13, 5, '13/01/2024', '12:00')
+def menu_cartao_embarque(nome_passageiro, numero_voo, cod_reserva, data, hora):
+    meu_cartao_embarque = Cartao_embarque(nome_passageiro, numero_voo, cod_reserva, data, hora)
+    print(meu_cartao_embarque)
+    while True:
+        opcao = int(input('''\nEscolha uma opção:
+    [ 1 ] Realizar check-in
+    [ 2 ] Alterar assento
+    [ 3 ] Terminar
+    --> '''))
     
-    print(cartao_1)
+        if opcao == 1:
+            meu_cartao_embarque.realiza_checkin()
+        elif opcao == 2:
+            assento = int(input('Digite o número do novo assento: '))
+            meu_cartao_embarque.altera_assento(assento)
+        elif opcao == 3:
+            break
+        return meu_cartao_embarque   
+    
+def main():
+    cartao_1 = menu_cartao_embarque('Carlos André', 13, 'DEV202', '13/01/2024', '12:00')
+    cartao_2 = menu_cartao_embarque('Julia', 15, 'ABC123', '02/05/2024', '20:00')
+    cartao_3 = menu_cartao_embarque('João', 20, 'DEF456', '5/08/2024', '17:00')
+    
+    print('======== Relatório de cartões de embarque ========\n')
+    print(cartao_1 + '\n')
+    print(cartao_2+ '\n')
+    print(cartao_3+ '\n')
 
 if __name__=='__main__':
     main()
