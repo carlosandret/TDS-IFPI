3700530a6adaaf51f7668b2fa8cce74cf0a9d841
Carlos André <161266479+carlosandret@users.noreply.github.com>
2024-11-24 17:16:31 +0000

    Auterações

---
+++ TDS_2/Orientação a Objetos-POO/teste.py
@@ -0,0 +1,5 @@
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite mais um número: '))
+
+print(f'A soma entre {n1} e {n2} é {n1+n2}'')
+print(f'A multiplicação entre {n1} e {n2} é {n1*n2}'')
