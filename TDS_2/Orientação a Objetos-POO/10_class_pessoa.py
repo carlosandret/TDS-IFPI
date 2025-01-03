380aaf7468dbbfb93af0de9691eaffb979e4403d
Carlos André <161266479+carlosandret@users.noreply.github.com>
2025-01-03 10:09:50 -0300

    Update 10_class_pessoa.py

--- TDS_2/Orientação a Objetos-POO/10_class_pessoa.py
+++ TDS_2/Orientação a Objetos-POO/10_class_pessoa.py
@@ -1,10 +1,11 @@
+# Funções para alterar a cor do texto e dar destaque
 def vermelho(texto):
     return f"\033[33;31m{texto}\033[m"
 def verde(texto):
     return f"\033[33;32m{texto}\033[m"
 
 class Pessoa:
-  def __init__(self,nome,idade,peso,altura,sexo,estado="vivo",est_civil="solteiro",mãe=None):
+  def __init__(self,nome,idade,peso,altura,sexo,estado="vivo(a)",est_civil="solteiro(a)",mãe=None):
     self.__nome = nome
     self.__idade = idade
     self.__peso = peso
@@ -30,7 +31,7 @@ class Pessoa:
     return self.__est_civil
   @nome.setter
   def nome(self,valor):
-    if self.__est_civil == "casado":
+    if self.__est_civil == "casado(a)":
       nome_antigo = self.__nome.split(" ")
       nome_conjuge = self.__conjuge.nome.split(" ")
       novo_nome = valor.split(" ")
@@ -43,16 +44,18 @@ class Pessoa:
   
 # 1. Altere o código de forma a impedir que uma pessoa "casada"  possa se casar novamente. Exceções: Quando a pessoa se tornar "viúva" ou "divorciada"
   def casar(self,conjuge):
-    if self.__est_civil != "casado(a)":
-        self.__est_civil = "casado(a)"
-        #Confere se o conjuge é uma pessoa
-        if type(conjuge)==Pessoa:
-            self.__conjuge = conjuge
-            # Modifica os atributos do cônjuge
-            self.__conjuge.__est_civil = "casado(a)"
-            self.__conjuge.__conjuge = self
-        print(verde(f'# {self.__nome} e {self.__conjuge.__nome} se casaram com sucesso!'))
-    else: print(vermelho('# Voçê já é casado, não pode casar novamente!!'))
+    if self.__estado != 'morto(a)':
+      if self.__est_civil != "casado(a)":
+          self.__est_civil = "casado(a)"
+          #Confere se o conjuge é uma pessoa
+          if type(conjuge)==Pessoa:
+              self.__conjuge = conjuge
+              # Modifica os atributos do cônjuge
+              self.__conjuge.__est_civil = "casado(a)"
+              self.__conjuge.__conjuge = self
+          print(verde(f'# {self.__nome} e {self.__conjuge.__nome} se casaram com sucesso!'))
+      else: print(vermelho('# Voçê já é casado(a), não pode casar novamente!!'))
+    else: print(vermelho('# Não é possivel ter filhos enquanto se está morto!'))
 
   def morrer(self):
     if self.__estado != 'morto(a)':
@@ -71,31 +74,42 @@ class Pessoa:
             conjuge.__conjuge = None
             print(verde(f'# {self.__nome} se divorciou com sucesso!'))
         else:
-            print(vermelho('# O cônjuge deve ser um objeto do tipo pessoa!'))
+          print(vermelho('# O cônjuge deve ser um objeto do tipo pessoa!'))
     else:
         print(vermelho('# Você já é divorciado(a)!'))
 
-# 2. Crie um método: "ter_filhos". Condição para este método ser executado:
-# a) Pessoa ser do sexo: "Feminino"
-# b) Segunda pessoa ser do sexo: "Masculino" . Ex: maria.ter_filho(joao), nesse caso maria é um objeto do tipo "Pessoa" do sexo: "feminino" e joao é um objeto do tipo "Pessoa" do sexo: "masculino". Esse método tem que retornar um novo objeto do tipo "Pessoa" 
-# c) acrescentar os atributos: Pai e Mãe (tipo: Pessoa).
-# d) Inserir na lista: filhos, todos os objetos gerados.
-
   def ter_filhos(self,pessoa):
     if self.__estado != 'morto(a)':
+      if type(pessoa)==Pessoa:
         if self.__sexo == 'F' and pessoa.__sexo == 'M':
-            filho = Pessoa('filho',0,4,0.40,'M')
-            filho.__pai = pessoa
-            filho.__mãe = self
+            filho = Pessoa('filho',0,4,0.04,'M')
+            filho.__pai = pessoa.__nome
+            filho.__mãe = self.__nome
             self.filhos.append(filho)
             print(verde(f'# {self.__nome} teve um filho com sucesso!'))
             return filho       
         else:
             print(vermelho('# Só é possível que uma Mulher tenha filho com um homem!'))
+      else:
+        print(vermelho('# O parceiro deve ser um objeto do tipo pessoa!'))
     else: print(vermelho('# Não é possivel ter filhos enquanto se está morto!'))
         
   def adotar_filhos(self,criança): #condição: criança ser órfã.
-    pass
+    if self.__estado != 'morto(a)':
+      if type(criança)==Pessoa:
+        if criança.__pai == None and criança.__mãe ==None:
+          if self.__sexo == 'M':
+            criança.__pai_adotivo = self.__nome
+            criança.__mãe_adotiva = self.__conjuge
+          else:
+            criança.__pai_adotivo = self.__conjuge
+            criança.__mãe_adotiva = self.__nome          
+        else:
+          print(vermelho('# A criança já possui pais então não pode ser adotada!'))
+      else:
+        print(vermelho('# O parceiro deve ser um objeto do tipo pessoa!'))
+    else:
+      print(vermelho('# Não é possivel ter filhos enquanto se está morto!'))
 
   def __str__(self):
     x = f'{'-'*10} {self.__nome} {'-'*10}' 
@@ -109,42 +123,48 @@ def main():
     maria = Pessoa("Maria",30,65,1.7,'F')
     joao = Pessoa("João",25,70,1.8,'M')
     clara = Pessoa('Clara',26,64,1.65,'F')
-    criança  = Pessoa('Luis', 10,40,1.40,'M')
+    criança  = Pessoa('Filho adotado', 10,40,1.40,'M')
     
     print(maria) 
     print(joao)
     
-    # Maria casa com joão
+    # Maria casa com João
     maria.casar(joao)
     print(maria)
     print(joao)
+    
+    # João tenta se casar com Clara
+    joao.casar(clara)
+    print(joao)
+    
+    # João tem umm filho com Clara
+    filho_clara = clara.ter_filhos(joao)
+    print(filho_clara)
+    print(verde(f'Quantidade de filhos de Clara: {len(clara.filhos)}\n'))
     
     # Maria e joao se divorciam
     maria.divorciar(joao)
     print(maria)
     print(joao)
     
-    # Maria tem um filho com joao
-    filho_maria = maria.ter_filhos(joao)
-    print(filho_maria)
-    print('\n')
-    print(maria.filhos)
+    # Maria tenta se divorciar novamente
+    maria.divorciar(joao)
     
+    # Maria adota um filho
+    maria.adotar_filhos(criança)
+    print(criança)
+    print(maria)
+    
+    # Clara morre 
+    clara.morrer()
+    print(clara)
+    
+    # Clara tenta se casar com João depois de morta
+    clara.casar(joao)
+    print(clara)
+    
+    # João tenta ter um filho com Clara depois de morta
+    joao.ter_filhos(clara)
     
 if __name__=="__main__":
     main()
-
-####### execução ########
-
-# maria = Pessoa("Maria",30,65,1.7,'F', Pessoa("Francisca",65,60,1.6,'F')) # maria -> solteira
-# joao = Pessoa(...) # joão -> solteiro
-# maria.casar(joao) # joão e maria -> casado
-# ana = Pessoa(...)
-# pedro = joao.ter_filhos(ana)
-# joao.casar(ana) # não é possivel pois joão já é casado com maria
-# maria.morrer() # maria para para o estado de morto.
-# joao.casar(ana) # joao e ana -> casado
-# joao.ter_filhos(maria) # Erro! maria está morta.
-# julia = ana.ter_filhos(joao)
-
-#simular processo de adoção
