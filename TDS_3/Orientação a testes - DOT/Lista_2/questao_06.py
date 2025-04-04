2b289e384b7dccbb701fc03b725868e55c79f4a8
Carlos André <161266479+carlosandret@users.noreply.github.com>
2025-04-03 22:01:45 -0300

    dgshgdsh

--- TDS_3/Orientação a testes - DOT/Lista_2/questao_06.py
+++ TDS_3/Orientação a testes - DOT/Lista_2/questao_06.py
@@ -13,19 +13,35 @@ def gera_lista(qtd_num):
             lista.append(x)
     return lista
 
+# Função querecebe uma lista de quantidade e outra de preço e gera outra lista com o faturamento de cada produto
 def calcula_faturamento(lista_qtd, lista_valores):
     faturamentos = []
     for i in range(len(lista_qtd)):
         faturamentos.append(lista_qtd[i]*lista_valores[i])
     return faturamentos
 
+def qtd_abaixo_da_media(lista, media):
+    resultado = []
+    for i in lista:
+        if i < media:
+            resultado.append(i)
+    return len(resultado)
+
+def media_lista(lista):
+    return sum(lista)/len(lista)
+
 def main():
     while True:
-        lista_1 = []
-        lista_2 = []
-        if type(lista_1) and type(lista_2) != str:
-            
-
+        quantidade_p = gera_lista(20)
+        preco_p = [20, 30, 10.5, 28.7, 36.4, 9.34, 6.45, 35.6, 34.8,3.5, 20, 30, 10.5, 28.7, 36.4, 5.8, 9.34, 6.45, 35.6, 34.8]
+        if type(quantidade_p) and type(preco_p) != str:
+            lista_faturamentos = calcula_faturamento(quantidade_p, preco_p)        
+            media_faturamento = media_lista(lista_faturamentos)
+            print(f'''Faturamento de cada produto em reais: {lista_faturamentos}
+        \nFaturamento total: R$ {sum(lista_faturamentos):.2f}
+        \nMédia dos faturamentos: R$ {media_faturamento:.2f}
+        \nQuantidade de fturamentos abaixo da média: {qtd_abaixo_da_media(lista_faturamentos, media_faturamento)}''')
+            break
         else:
             print("Entrada inválida! a lista deve ser de números, tente novamente!")
 
