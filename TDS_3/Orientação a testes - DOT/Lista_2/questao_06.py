70f3f90d527f6f000a1ed59dc3268bd99ea39ab7
Carlos André <161266479+carlosandret@users.noreply.github.com>
2025-04-06 12:24:17 -0300

    pull

--- TDS_3/Orientação a testes - DOT/Lista_2/questao_06.py
+++ TDS_3/Orientação a testes - DOT/Lista_2/questao_06.py
@@ -8,18 +8,19 @@ from random import randint
 def gera_lista(qtd_num):
     lista = []
     while len(lista) < qtd_num:
-        x = randint(1, 10)
+        x = randint(1, 60)
         if x not in lista:
             lista.append(x)
     return lista
 
-# Função querecebe uma lista de quantidade e outra de preço e gera outra lista com o faturamento de cada produto
+# Função que recebe uma lista de quantidade e outra de preço e gera outra lista com o faturamento de cada produto
 def calcula_faturamento(lista_qtd, lista_valores):
     faturamentos = []
     for i in range(len(lista_qtd)):
         faturamentos.append(lista_qtd[i]*lista_valores[i])
     return faturamentos
 
+# Função que recebe uma lista e um valor de média e retorna quantos valores da lista são menores do que a média 
 def qtd_abaixo_da_media(lista, media):
     resultado = []
     for i in lista:
@@ -27,20 +28,21 @@ def qtd_abaixo_da_media(lista, media):
             resultado.append(i)
     return len(resultado)
 
+# Função que calcula a media dos elementos de uma lista
 def media_lista(lista):
     return sum(lista)/len(lista)
 
 def main():
     while True:
         quantidade_p = gera_lista(20)
-        preco_p = [20, 30, 10.5, 28.7, 36.4, 9.34, 6.45, 35.6, 34.8,3.5, 20, 30, 10.5, 28.7, 36.4, 5.8, 9.34, 6.45, 35.6, 34.8]
-        if type(quantidade_p) and type(preco_p) != str:
+        preco_p = [7, 5, 10.5, 8.7, 6.4, 9.34, 6.45, 9.6, 4.8, 3.5, 10, 6, 10.5, 8.7, 6.4, 5.8, 9.34, 6.45, 5.6, 4.8]
+        if type(quantidade_p) != str and type(preco_p) != str:
             lista_faturamentos = calcula_faturamento(quantidade_p, preco_p)        
             media_faturamento = media_lista(lista_faturamentos)
-            print(f'''Faturamento de cada produto em reais: {lista_faturamentos}
+            print(f'''\nFaturamento de cada produto em reais: {lista_faturamentos}
         \nFaturamento total: R$ {sum(lista_faturamentos):.2f}
         \nMédia dos faturamentos: R$ {media_faturamento:.2f}
-        \nQuantidade de fturamentos abaixo da média: {qtd_abaixo_da_media(lista_faturamentos, media_faturamento)}''')
+        \nQuantidade de faturamentos abaixo da média: {qtd_abaixo_da_media(lista_faturamentos, media_faturamento)}''')
             break
         else:
             print("Entrada inválida! a lista deve ser de números, tente novamente!")
