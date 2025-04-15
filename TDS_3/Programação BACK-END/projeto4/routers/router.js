24453ef856d3732e657e97cf15c56c0b444040c3
Carlos André <161266479+carlosandret@users.noreply.github.com>
2025-04-15 14:40:15 -0300

    ultimo comit

--- TDS_3/Programação BACK-END/projeto4/routers/router.js
+++ TDS_3/Programação BACK-END/projeto4/routers/router.js
@@ -5,11 +5,27 @@ router.get('/', (req, res) => {
     res.render('home')
 })
 
-router.get('historico', (req, res) => {
-    res.render('historico')
+let registrosFeitos = [];
+router.get('/historico', (req, res) => {
+    res.render('historico', {registrosFeitos: registrosFeitos})
 })
 
+router.get('/registro', (req, res) => {
+    res.render('registro')
+})
 
-
-
+router.post('/registro', (req, res) => {
+    disciplina = req.body.disciplina;
+    nota1 = parseFloat(req.body.nota1);
+    nota2 = parseFloat(req.body.nota2);
+    media = (nota1 + nota2 )/ 2;
+    registro = {
+        disciplina : disciplina,
+        n1 : nota1,
+        n2 : nota2,
+        media : media
+    }
+    registrosFeitos.push(registro);
+    res.render('historico', {registrosFeitos});
+})
 module.exports = router
