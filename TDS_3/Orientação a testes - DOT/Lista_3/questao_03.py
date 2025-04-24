94333cab3b0993e0588db3f63ab0d0ad3c2a5ce5
Carlos André <161266479+carlosandret@users.noreply.github.com>
2025-04-24 14:21:52 -0300

    sas

--- TDS_3/Orientação a testes - DOT/Lista_3/questao_03.py
+++ TDS_3/Orientação a testes - DOT/Lista_3/questao_03.py
@@ -10,39 +10,24 @@ def gera_lista(qtd_num, max_valor):
         lista.append(x)
     return lista
 
-# Função que retira os itens repetidos da lista, retorna uma nova lista sem repetição
-def tira_repetido(lista):
-    nova_lista = []
-    for i in lista:
-        if i not in nova_lista:
-            nova_lista.append(i)
-    return nova_lista
-
-# Função que conta quantas vezes cada item aparece na lista, retorna a quantidade 
-def conta_instancia(item, lista):
-    cont = 0
-    for i in lista:
-        if i == item:
-            cont += 1
-    return cont
-
-# Função que imprime a quantidade de vezes que cada número aparece na lista original
-def conta_quantidade_lista(lista_original, numeros):
-    for i in numeros:
-        qtd = conta_instancia(i, lista_original)
-        print(f"O número {i} aparece {qtd}",
-        "vez" if qtd == 1 else "vezes")
+# Função que recebe uma lista e retorna a maior soma de um segmento de 2 valores
+def calc_maior_soma(lista):
+    maior_soma = 0
+    for i in range(len(lista) - 1):
+        soma = lista[i] + lista [i + 1]
+        if soma > maior_soma:
+            maior_soma = soma            
+    return maior_soma
     
 def main():
     while True:
         try:            
-            lista_original = gera_lista(10, 10) 
-            lista_sem_repeticao = tira_repetido(lista_original)
-            
-            print(f"\nLista original: {lista_original}\n")
-            
-            conta_quantidade_lista(lista_original, lista_sem_repeticao)    
-            break
+            lista_original = gera_lista(10, 10)
+            maior_soma = calc_maior_soma(lista_original) 
+
+            print(f"\nLista original: {lista_original}")
+            print(f"\nMaior de um segmento com dois valores: {maior_soma}")
+            break   
         except:
             print("ERRO: Entrada inválida, digite apenas números, tente novamente!")
 if __name__=='__main__':
--- TDS_3/Orientação a testes - DOT/Lista_3/questao_04.py
+++ TDS_3/Orientação a testes - DOT/Lista_3/questao_04.py
@@ -1,2 +1,34 @@
 # 4) Escreva uma função que recebe uma lista com n números inteiros, e determina a
 # maior soma de qualquer seguimento da lista. Ex: Ex: [5, -2, -2, -7, 3, 15, 10, -3, 9, -6, 4, 1] = 34
+
+# Função que gera uma lista de números aletórios, recebe a quantidade de números e o valor maximo da sequência
+from random import randint
+def gera_lista(qtd_num, max_valor):
+    lista = []
+    for i in range(qtd_num):
+        x = randint(0, max_valor)
+        lista.append(x)
+    return lista
+
+# Função que recebe uma lista e retorna a maior soma de qualquer segmento da lista
+def calc_maior_soma(lista):
+    maior_soma = 0
+    for i in range(len(lista) - 1):
+        soma = lista[i] + lista [i + 1]
+        if soma > maior_soma:
+            maior_soma = soma            
+    return maior_soma
+    
+def main():
+    while True:
+        try:            
+            lista_original = gera_lista(10, 10)
+            maior_soma = calc_maior_soma(lista_original) 
+
+            print(f"\nLista original: {lista_original}")
+            print(f"\nMaior de um segmento com dois valores: {maior_soma}")
+            break   
+        except:
+            print("ERRO: Entrada inválida, digite apenas números, tente novamente!")
+if __name__=='__main__':
+    main()
