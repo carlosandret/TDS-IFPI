e785ac716cef5fa1745c1183b14162941816de01
Carlos André <161266479+carlosandret@users.noreply.github.com>
2025-05-07 15:17:03 -0300

    L1_q1_dot

--- TDS_3/Orientação a testes - DOT/Lista_1/questao_01.py
+++ TDS_3/Orientação a testes - DOT/Lista_1/questao_01.py
@@ -1,21 +1,39 @@
 # 1. Faça uma função que recebe um número inteiro por parâmetro e retorna verdadeiro se ele for par e falso se for ímpar.
 
-def par_impar(n):
-    if n % 2 == 0:
-        return True
+def confere_par(n):
+    if type(n) == int:
+        if n % 2 == 0:
+            return True
+        elif n % 3 == 0:
+            return False
     else:
-        return False
+        return Exception
 
+def main():
+    while True:
+        try:
+            num = int(input("\nDigite um número: "))
+            if confere_par(num) == True:
+                print(f"\nO número {num} é par")
+            else:
+                print(f"\nO número {num} é ímpar")
+                break
+        except:
+            print("ERRO: Digite um número válido!!")
 
-while True:
-    try:
-        num = int(input("\nDigite um número: "))
-        if par_impar(num) == True:
-            print(f"\nO número {num} é par")
-        else:
-            print('f\nO número {num} é ímpar')
-            break
-    except:
-        print("Digite um número válido!!")
+def teste():
+    assert confere_par(4) == True
+    assert confere_par(3) == False
+    assert confere_par(-3) == False
+    assert confere_par('e') == Exception
+    assert confere_par(3.5) == Exception
+    print("Todos os testes passaram!")
 
+# Chave para ligar o modo de testes
+teste_mode = True
 
+if __name__ == "__main__":
+    if teste_mode:
+        teste()
+    else:
+        main()
