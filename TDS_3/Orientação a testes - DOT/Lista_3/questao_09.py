bfc1176464084be8cffd77bdf372ec6ca1578bca
Carlos André <161266479+carlosandret@users.noreply.github.com>
2025-05-07 15:54:32 -0300

    l1_q9_dot

--- TDS_3/Orientação a testes - DOT/Lista_3/questao_09.py
+++ TDS_3/Orientação a testes - DOT/Lista_3/questao_09.py
@@ -1,2 +1,20 @@
 # 9) Escreva uma função que recebe uma lista com n números inteiros, retornar uma
 # lista eliminando todas as ocorrências de valores repetidos. Ex: [5, 4, 5, 7, 3, 4] = [7, 3]
+
+def retira_repetido(lista):
+    n_lista = []
+    for i in lista:
+        ocorrencia = 0
+        for c in lista:
+            if i == c:
+                ocorrencia += 1
+        if ocorrencia < 2:
+            n_lista.append(i)
+    return n_lista
+
+def main():
+    lista = [5, 4, 5, 7, 3, 4]
+
+    print(retira_repetido(lista))
+if __name__=="__main__":
+    main()
